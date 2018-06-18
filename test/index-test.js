0c5b507b35a8244acc67a15d3789ec7aa32fb61d
mamadou.m.diakhate@gmail.com <mamadou.m.diakhate@gmail.com>
2018-06-18 04:53:12 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -29,14 +29,6 @@ describe('sayHiToGrandma(string)', function() {
   function sayHiToGrandma(string){
     if (string === string.toLowerCase()) return "I can't hear you!";
     else if (string === "I love you, Grandma.") return "I love you, too.";
-    
+    else if (string === string.toUpperCase()) return "YES INDEED!";
   });
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+});
