1d4156a1f6a115fabd2caab6a1b18cc36f4cde45
mamadou.m.diakhate@gmail.com <mamadou.m.diakhate@gmail.com>
2018-06-18 04:28:18 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 function shout(string){
   return string.toUpperCase();
 }
-shout('hello')
+shout('hello');
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
